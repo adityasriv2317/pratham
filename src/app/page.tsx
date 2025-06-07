def938ab64e885e26c4b8d10f1e1ff6abba816f6
adityasriv2317 <adityaxia9237@gmail.com>
2025-06-07 22:39:16 +0530

    loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 "use client";
 
+import Loader from "@/components/Loader";
 import { useAuthRedirect } from "@/hooks/useAuthRedirect";
 
 export default function Page() {
   useAuthRedirect();
-  return null;
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="h-20 w-2 border-2 border-white border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    </div>
+  );
 }
